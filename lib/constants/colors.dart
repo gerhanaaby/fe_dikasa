6119cb1344173026fff745a26b3ca12fc992056c
Reynaldi Irfan Anwar <reynaldi.i.anwar@banksinarmas.com>
2023-03-11 11:19:29 +0700

    fixing hex color [Done]

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -10,44 +10,44 @@ const textFormColorGrey = Color.fromRGBO(112, 112, 112, 0.45);
 const textFormColorDarkGrey = Color.fromRGBO(116, 116, 116, 1);
 const bgTextFromGrey = Color.fromRGBO(238, 238, 238, 1);
 //orange
-const orangeBtnMain = Color(0xFF8227);
+const orangeBtnMain = Color.fromARGB(255, 255, 130, 39);
 
 //white
-const whiteMain = Color(0xFFFFF);
+const whiteMain =  Color.fromARGB(255, 255, 255, 255);
 
 //grey
-const greyTextMainH1 = Color(0x656565);
-const greyFormField = Color(0xEEEEEE);
-const greyProductName = Color(0x726F6F);
-const greyHeaderLogin = Color(0x8F8F8F);
-const greyUnSelectOptionButtonPelanggan = Color(0xD9D9D9); //also : headerButton (left)
-const greyUnSelectOptionButtonMinumanTambahan = Color(0xEBEBEB); //also : OTP
-const greyHeader = Color(0xF6F6F6); //also : nama user (header)
-const greyCardProduct = Color(0xEDEDED);
-const greyBgListListOrder = Color(0xF6F6F6);
-const greyBgListTotalPesanan = Color(0xDBDADA);
-const greyBgBtnCollection = Color(0xF8F8F8);
-const greyBgHeaderPembayaranStage2 = Color(0xF9F9F9);
-const greyBgHeaderPembayaranStage3 = Color(0xF6F6F6);
-const greyPlaceHolder = Color(0xAFAFAF);
+const greyTextMainH1 = Color.fromARGB(255, 101, 101, 101);
+const greyFormField = Color.fromARGB(255, 238, 238, 238);
+const greyProductName = Color.fromARGB(255, 114, 111, 111);
+const greyHeaderLogin = Color.fromARGB(255, 143, 143, 143);
+const greyUnSelectOptionButtonPelanggan = Color.fromARGB(255,217, 217, 217);//also : headerButton (left)
+const greyUnSelectOptionButtonMinumanTambahan = Color.fromARGB(255, 235, 235, 235);//also : OTP
+const greyHeader = Color.fromARGB(255, 246, 246, 246);//also : nama user (header)
+const greyCardProduct = Color.fromARGB(255, 237, 237, 237);
+const greyBgListListOrder = Color.fromARGB(255, 246, 246, 246);
+const greyBgListTotalPesanan = Color.fromARGB(255, 219, 218, 218);
+const greyBgBtnCollection = Color.fromARGB(255, 248, 248, 248);
+const greyBgHeaderPembayaranStage2 = Color.fromARGB(255, 249, 249, 249);
+const greyBgHeaderPembayaranStage3 = Color.fromARGB(255, 246, 246, 246);
+const greyPlaceHolder = Color.fromARGB(255, 175, 175, 175);
 
 //blue
-const blueHeaderButton = Color(0x077CD3); //also : btn_lockScreen
-const blueSelectedOptionButton = Color(0x077CD3);
-const blueBgHeaderPembayaranStage1 = Color(0x2C5E7A);
-const blueBgHeaderPembayaranStage2 = Color(0x367AA0);
-const blueBgHeaderPembayaranStage3 = Color(0x529DC8);
-const blueBtnMain = Color(0x077CD3);
-const blueTextColorMain = Color(0x097BC2);
-const blueSelectedBtnPembayaranStage2 = Color(0x27A7EF);
-const blueBgHeaderModalPopUp = Color(0x367AA0);
+const blueHeaderButton = Color.fromARGB(255, 7, 124, 211);//Color(0x077CD3); //also : btn_lockScreen
+const blueSelectedOptionButton = Color.fromARGB(255, 7, 124, 211);//Color(0x077CD3);
+const blueBgHeaderPembayaranStage1 = Color.fromARGB(255, 44, 94, 122);//Color(0x2C5E7A);
+const blueBgHeaderPembayaranStage2 = Color.fromARGB(255, 54, 122, 160);//Color(0x367AA0);
+const blueBgHeaderPembayaranStage3 = Color.fromARGB(255, 82, 157, 200);//Color(0x529DC8);
+const blueBtnMain = Color.fromARGB(255, 7, 124, 211);//Color(0x077CD3);
+const blueTextColorMain = Color.fromARGB(255, 7, 124, 211);//Color(0x097BC2);
+const blueSelectedBtnPembayaranStage2 = Color.fromARGB(255, 39, 167, 239);//Color(0x27A7EF);
+const blueBgHeaderModalPopUp = Color.fromARGB(255, 54, 122, 160);//Color(0x367AA0);
 
 //red
-const redLowOfStock = Color(0x863131);
-const redBtnMain = Color(0x9D1414);
+const redLowOfStock = Color.fromARGB(255, 134, 49, 49);//Color(0x863131);
+const redBtnMain = Color.fromARGB(255, 157, 20, 20);//Color(0x9D1414);
 
 //green
-const greenTextColorMain = Color(0x218831);
+const greenTextColorMain = Color.fromARGB(255, 33, 136, 49);//Color(0x218831);
 
 
 const kPrimaryGradientColor = LinearGradient(
